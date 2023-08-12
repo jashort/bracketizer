4c7318326bcb01edd72d249a9bb9181c3bb940e3
Jason Short <jason@sheersky.com>
2023-08-12 09:28:51 -0700

    Store in progress votes in session, persist at the end

--- bracketizer/vote.py
+++ bracketizer/vote.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from flask import request, session, flash, render_template, redirect, Blueprint, url_for, abort
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, RadioField, HiddenField
-from sqlalchemy.dialects.sqlite import insert as sqlite_upsert
 
 from bracketizer.models import Bracket, Vote, db
 from bracketview import BracketView
@@ -32,7 +31,19 @@ def vote():
             flash("No peeking! This bracket isn't open for voting")
             return redirect(url_for('index'))
 
-        votes = Vote.query.filter_by(username=session['username'], bracket_id=my_bracket.id)
+        user_vote_count = Vote.query.filter_by(username=session['username'], bracket_id=my_bracket.id).count()
+        if user_vote_count > 0:
+            flash(f"It looks like you've already voted in {bracket_name}")
+            return redirect(url_for('index'))
+
+        if 'votes' in session:
+            votes = [Vote(username=session['username'],
+                          bracket_id=my_bracket.id,
+                          round_number=v['r'],
+                          question_number=v['q'],
+                          choice=v['c']) for v in session['votes']]
+        else:
+            votes = []
 
         bv = BracketView(my_bracket, votes)
         my_round = bv.get_round(round_number)
@@ -40,18 +51,15 @@ def vote():
         my_form.choice.choices = bv.get_question(round_number, question_number)
 
         if my_form.validate_on_submit():
-            # Upsert votes for a given user/bracket/round/question. You can change it as long
-            # as voting is still open.
-            stmt = sqlite_upsert(Vote).values([{"username": session['username'], "bracket_id": my_bracket.id,
-                                                "round_number": round_number, "question_number": question_number,
-                                                "choice": my_form.choice.data}
-                                               ])
-            stmt = stmt.on_conflict_do_update(
-                index_elements=[Vote.username, Vote.bracket_id, Vote.round_number, Vote.question_number],
-                set_={"choice": my_form.choice.data}
+            if 'votes' not in session or session['votes'] is None:
+                session['votes'] = []
+            session['votes'].append(
+                {"b_id": my_bracket.id,
+                 "r": round_number,
+                 "q": question_number,
+                 "c": my_form.choice.data}
             )
-            db.session.execute(stmt)
-            db.session.commit()
+            session.modified = True
 
             if question_number < my_round.total_questions():
                 return redirect(
@@ -59,7 +67,37 @@ def vote():
             elif round_number < my_bracket.total_rounds() - 1:
                 return redirect(url_for('vote.vote', bracket=bracket_name, round=round_number + 1, question=1))
             else:
-                flash('All done!')
+                # Should have votes for every question, persist to database
+                # From the session, filter votes that are for this bracket vs not
+                to_keep_in_session = []
+                to_save = []
+                for v in session['votes']:
+                    if v['b_id'] == my_bracket.id:
+                        to_save.append(v)
+                    else:
+                        to_keep_in_session.append(v)
+
+                # Convert to ORM objects and save them in the database
+                db_votes = []
+                for v in to_save:
+                    db_votes.append(
+                        Vote(username=session['username'],
+                             bracket_id=my_bracket.id,
+                             round_number=v['r'],
+                             question_number=v['q'],
+                             choice=v['c']
+                             ))
+                db.session.add_all(db_votes)
+                db.session.commit()
+
+                # Remove votes for this bracket from the session. There's a
+                # race condition here (session is saved between browser tabs)
+                # but practically I don't expect that to matter. Is someone really
+                # switching back and forth between tabs voting in different brackets in
+                # the same session? Doubtful.
+                session['votes'] = to_keep_in_session
+                session.modified = True
+                flash('Saved!')
                 return redirect(url_for('bracket.bracket', bracket=bracket_name))
         return render_template('vote.html',
                                form=my_form,
